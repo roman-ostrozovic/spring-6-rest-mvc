12d6cab5d2845b7abf31c176e3dcfbedef55a9e3
Roman Ostrozovic <roman.ostrozovic@gmail.com>
2023-05-17 09:08:09 +0200

    adding list example to the service impl adding get list of beers adding get beer by id

--- src/main/java/guru/springframework/spring6restmvc/services/BeerService.java
+++ src/main/java/guru/springframework/spring6restmvc/services/BeerService.java
@@ -3,9 +3,12 @@ package guru.springframework.spring6restmvc.services;
 import guru.springframework.spring6restmvc.modul.Beer;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.UUID;
 
 @Service
 public interface BeerService {
+
+    List<Beer> listBeers();
     Beer getBeerById(UUID id);
 }
